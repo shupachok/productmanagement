680c5b4da9035cf11291869a5dcf9f9e3d1f589b
SUPACHOK\shupa <shupachok@gmail.com>
2024-04-28 15:20:40 +0700

    [add] authentication for order service

---
+++ OrdersService/src/main/java/com/sp/ordersservice/security/WebSecurity.java
@@ -0,0 +1,52 @@
+package com.sp.ordersservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableMethodSecurity(prePostEnabled = true)
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+	private Environment environment;
+
+	public WebSecurity(Environment environment) {
+		this.environment = environment;
+	}
+
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		
+		AuthenticationManagerBuilder authenticationManagerBuilder = 
+				http.getSharedObject(AuthenticationManagerBuilder.class);
+		
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+		http
+		.authorizeHttpRequests(
+				auth -> auth.requestMatchers("/h2-console/**").permitAll()
+		)
+		.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated()
+		)
+        .addFilter(new AuthorizationFilter(authenticationManager, environment))
+        .authenticationManager(authenticationManager)
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .csrf(conf -> conf.disable())
+		.headers(conf -> conf.frameOptions(foption -> foption.disable()));
+		
+		
+		return http.build();
+
+	}
+
+}
